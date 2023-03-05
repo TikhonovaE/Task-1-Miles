a9745a98a57c770ab33c6e45381b9dbdbfc1d32b
Tikhonova Evgeniia <tikhonovae555@gmail.com>
2023-03-05 20:13:18 +0700

    Changes added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int ticketCost = 10_000;
+        int bonusRub = 20;
+        int bonusMiles = ticketCost / bonusRub;
+        System.out.println("Бонусные мили " + bonusMiles);
     }
 }
